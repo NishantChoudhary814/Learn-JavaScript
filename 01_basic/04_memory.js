3bfed35a23b3fda96e222ea83bae281affdaa1c6
NishantChoudhary814 <98717344+NishantChoudhary814@users.noreply.github.com>
2024-08-30 20:17:33 +0000

    learn

---
+++ 01_basic/04_memory.js
@@ -0,0 +1,18 @@
+ //stack (Primitive), Heap(Non-Primitive)
+let myName="Nishant Choudhary"
+let anotherName= myName
+
+anotherName="Tiger Khan"
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email:"user@goggle.com",
+    upid: "user@xyz"
+}
+
+let anotherUser= userOne
+anotherUser.email="abcxyz@gmail.com"
+console.log(userOne)
+console.log(anotherUser)
+
---
+++ 01_basic/05_string.js
@@ -0,0 +1,39 @@
+const name="Nishant_Choudhary"
+const repoCount=50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String("qwertyuiop");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString=gameName.substring(0,4)//non negative value 
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const str="      Tiger        "
+console.log(str)
+console.log(str.trim())
+
+const url ="https://nishant.com/nishant%20choudhary"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('tiger'))
+
+
+
+
+
+
+
+
